4e1b3c7f1c1f267923b9c38e7558de963c87fab7
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-08-20 19:45:04 +0530

    Time: 21 ms (54.50%) | Memory: 15.7 MB (43.83%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-
-    int solve(vector<int> & piles,vector<vector<vector<int>>> &dp,int turn ,int i , int m){
-        int n = piles.size();
-        if(i == n) return 0;
-        // turn == 1 -> bob
-        // turn == 0 ->alice
-
-        if(dp[turn][i][m] != -1){
-
-            return dp[turn][i][m];
-        }
-        int res = turn == 1 ? INT_MAX : -1;
-
-        int score =0;
-        for(int x = 1;x<= min(2*m,n-i);x++){
-            score+=piles[i+x-1];
-
+    int recur(int ind,int m,vector<int> &piles,int turn, vector<vector<vector<int>>> &dp){
+        if(ind == piles.size()) return 0;
+        if(dp[turn][ind][m] != -1) return dp[turn][ind][m];
+        int score = 0;
+        int res = turn == 0? -1:INT_MAX;
+        for(int i = 1;i<=min(2*m,(int)piles.size() - ind);i++){
+            score += piles[i + ind -1];
             if(turn == 0){
-                // apne ko alice ka max karana hai isliye max aur res == -1 tab initially
-                res = max(res,score+solve(piles,dp,1,i+x,max(m,x)));
-
-            }
+                // alice
+                res = max(res,score + recur(ind + i,max(m,i),piles,1,dp));
+            }   
             else{
-            // bob maa chudaye ...isliye uska minimum karna hai toh tab res == 100000 initially
-                res = min(res,solve(piles,dp,0,i+x,max(m,x)));
+                // bob
+                res = min(res,recur(ind + i,max(m,i),piles,0,dp));
             }
-
         }
-        return dp[turn][i][m] = res;
+        return dp[turn][ind][m] = res;
+        
     }
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<vector<int>>> dp(2,vector<vector<int>>(n+1,vector<int>(n+1,-1)));
-        return solve(piles,dp,0,0,1);      
+        vector<vector<vector<int>>> dp (2,vector<vector<int>> (n+1,vector<int>(n+1,-1)));
+        return recur(0,1,piles,0,dp);
     }
 };
