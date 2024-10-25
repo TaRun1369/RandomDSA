489478aa9d02c2ef0295c8bdd6b031e30741af52
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2024-10-25 11:58:31 +0530

    Time: 114 ms (84.89%) | Memory: 53.6 MB (64.56%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -8,24 +8,24 @@ public:
         for(auto it : folder){
             string temp = "";
             int f = 1;
-            for(int i = 0;i<it.size();i++){
+            for(int i = 0;i<it.size();){
                 // cout<<it<<" - "<< temp<<endl;
                 char ii = it[i];
                 if(ii == '/'){ 
+                    
+                    temp += it[i];i++;
+                }
+                else{
+                    while(i < it.size() && it[i] != '/'){
+                        temp += it[i];
+                        i++;
+                    }
+                    // i--;
+
                     if(mp.find(temp) != mp.end()){
                         f = 0;
                         break;
                     }
-                    temp += it[i];
-                }
-                else{
-                    // while(i < it.size() && it[i] != '/'){
-                        temp += it[i];
-                        // i++;
-                    // }
-                    // i--;
-
-                    
                     
                 }
             }
