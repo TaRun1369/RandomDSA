7eb99385efdd4a56adf4f6e95d6e92459a18b623
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2025-01-26 19:17:50 +0530

    Time: 571 ms (5.75%) | Memory: 289.7 MB (5.28%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,54 +1,54 @@
-// class Solution {
-// public:
-//     int n;
-//     int recur(int stockhold,int ind,vector<int> p,int mtran, vector<vector<vector<int>>> &dp){
-//         if(ind >= n || mtran == 0) return 0;
-
-//         int ans = 0;
-//         if(dp[ind][mtran][stockhold] != -1) return dp[ind][mtran][stockhold];
-//         if(stockhold == 0){
-//             ans = max(ans,recur(1,ind + 1,p,mtran,dp) - p[ind]);
-//         }
-//         else{
-//             ans = max(ans,recur(0,ind + 1,p,mtran-1,dp) + p[ind]);
-//         }
-//         ans = max(ans,recur(stockhold,ind + 1,p,mtran,dp));
-//         return dp[ind][mtran][stockhold] = ans;
-//     }
-//     int maxProfit(vector<int>& prices) {
-//         n = prices.size();
-//         vector<vector<vector<int>>> dp(n,vector<vector<int>>(3,vector<int>(2,-1)));
-//         return recur(0,0,prices,2,dp);
-//     }
-// };
 class Solution {
-    int solve(int index,int buy,int cnt,int n,vector<int> &prices,vector<vector<vector<int>>> &dp){
-        if(index==n){
-            return 0;
-        }
-        if(cnt==0){
-            return 0;
-        }
-        if(dp[index][buy][cnt]!=-1){
-            return dp[index][buy][cnt];
-        }
-        int profit=0;
-        if(buy){
-            int pick=-prices[index]+solve(index+1,false,cnt,n,prices,dp);
-            int notPick=0+solve(index+1,true,cnt,n,prices,dp);
-            profit=max(pick,notPick);
+public:
+    int n;
+    int recur(int stockhold,int ind,vector<int> &p,int mtran, vector<vector<vector<int>>> &dp){
+        if(ind >= n || mtran == 0) return 0;
+
+        int ans = 0;
+        if(dp[ind][mtran][stockhold] != -1) return dp[ind][mtran][stockhold];
+        if(stockhold == 0){
+            ans = max(ans,recur(1,ind + 1,p,mtran,dp) - p[ind]);
         }
         else{
-            int pick=prices[index]+solve(index+1,true,cnt-1,n,prices,dp);
-            int notPick=solve(index+1,false,cnt,n,prices,dp);
-            profit=max(pick,notPick);
+            ans = max(ans,recur(0,ind + 1,p,mtran-1,dp) + p[ind]);
         }
-        return dp[index][buy][cnt]=profit;
+        ans = max(ans,recur(stockhold,ind + 1,p,mtran,dp));
+        return dp[ind][mtran][stockhold] = ans;
     }
-public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(0,true,2,n,prices,dp);
+        n = prices.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(3,vector<int>(2,-1)));
+        return recur(0,0,prices,2,dp);
     }
 };
+// class Solution {
+//     int solve(int index,int buy,int cnt,int n,vector<int> &prices,vector<vector<vector<int>>> &dp){
+//         if(index==n){
+//             return 0;
+//         }
+//         if(cnt==0){
+//             return 0;
+//         }
+//         if(dp[index][buy][cnt]!=-1){
+//             return dp[index][buy][cnt];
+//         }
+//         int profit=0;
+//         if(buy){
+//             int pick=-prices[index]+solve(index+1,false,cnt,n,prices,dp);
+//             int notPick=0+solve(index+1,true,cnt,n,prices,dp);
+//             profit=max(pick,notPick);
+//         }
+//         else{
+//             int pick=prices[index]+solve(index+1,true,cnt-1,n,prices,dp);
+//             int notPick=solve(index+1,false,cnt,n,prices,dp);
+//             profit=max(pick,notPick);
+//         }
+//         return dp[index][buy][cnt]=profit;
+//     }
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n=prices.size();
+//         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+//         return solve(0,true,2,n,prices,dp);
+//     }
+// };
