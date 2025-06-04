1dca4da1cdffa01546e1bcd021847b98caed98a8
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2025-06-04 10:03:26 +0530

    Time: 41 ms (17.90%) | Memory: 33 MB (17.33%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    void recur(vector<int>& c, int target,int ind,set<vector<int>> &ans,vector<int> &temp){
-        if(target < 0 ) return;
-        if(target == 0){
-            ans.insert(temp);
+    map<pair<int, int>, vector<vector<int>>> dp; // memoization map
+
+    vector<vector<int>> recur(vector<int>& c, int target, int ind) {
+        if (target == 0) return {{}};
+        if (target < 0 || ind >= c.size()) return {};
+
+        if (dp.find({ind, target}) != dp.end()) return dp[{ind, target}];
+
+        vector<vector<int>> res;
+
+        // Include current element
+        vector<vector<int>> take = recur(c, target - c[ind], ind);
+        for (auto comb : take) {
+            comb.push_back(c[ind]);
+            res.push_back(comb);
         }
-        if( ind >= c.size())return;
-        temp.push_back(c[ind]);
-        recur(c,target - c[ind], ind,ans,temp);
-        temp.pop_back();
 
-        recur(c,target,ind + 1,ans,temp);
+        // Exclude current element
+        vector<vector<int>> skip = recur(c, target, ind + 1);
+        for (auto comb : skip) {
+            res.push_back(comb);
+        }
 
+        return dp[{ind, target}] = res;
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        set<vector<int>> ans;
-        vector<int> temp;
-        recur(candidates,target,0,ans,temp);
-        vector<vector<int>> anss(ans.begin(),ans.end());
-        return anss;
+        return recur(candidates, target, 0);
     }
 };
