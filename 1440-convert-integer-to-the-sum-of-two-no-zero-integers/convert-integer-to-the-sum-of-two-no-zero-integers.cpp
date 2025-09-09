828f42630871ae307bac9e877383873c3449ae8c
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2025-09-09 18:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.42%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     vector<int> getNoZeroIntegers(int n) {
-        for(int i = 1;i<=n/2;i++){
-            int a = i,b = n-i;
-            string sa = to_string(a),sb = to_string(b);
-            if(sa.find('0') == string::npos && sb.find('0') == string::npos){
-                 return {a,b};
+        int a=0,b=0;
+        int pos = 1;
+        while(n){
+            int rem = n%10;
+            if(rem == 0){
+                a += (1*pos);
+                b += (9*pos);
+                n-=10;
             }
+            else if(rem == 1 && n >= 10){
+                a += 2*pos;
+                b += 9*pos;
+                n-=10;
+            }
+            else{
+                a += (rem - 1)*pos;
+                b += 1*pos;
+            }
+            pos*=10;
+            n/=10;
         }
-        return {0,0};
+
+        return {a,b};
+
+
     }
 };
