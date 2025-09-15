fafc5fea611f5ba278a5294d36c3a490e353ccdb
Tarun Santani <98692373+TaRun1369@users.noreply.github.com>
2025-09-15 18:04:05 +0530

    Time: 19 ms (5.65%) | Memory: 8.7 MB (95.02%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        set<char> s;
+        for(auto it : brokenLetters){
+            s.insert(it);
+        }
+
+        // for(auto it : s) cout<<it<<endl;
+        int n = text.size();
+        int ans = 0,i = 0,wc = 0;
+        bool iscurr = false;
+        while(i < n){
+            if(text[i] == ' '){
+                wc++;
+                cout<<i<<" ";
+                if(iscurr == true){
+                    cout<<"true"<<endl;
+                    ans++;
+                } 
+                iscurr = false;
+                i++;
+            }
+
+            else if(s.find(text[i]) != s.end()){
+                iscurr = true;
+                while(i < n && text[i] != ' ') i++;
+                
+            }
+            else{
+                i++;
+            }
+            
+
+        }
+        wc++;
+        if(iscurr == true){
+                cout<<"true"<<endl;
+                ans++;
+        } 
+        return wc - ans;
+    }
+};
